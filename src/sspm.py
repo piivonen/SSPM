1b95592528168adfd3acc18c05f904d604b35647
anon <anon@deck.(none)>
2012-01-23 14:02:35 -0800

    Some header formatting.

--- src/sspm.py
+++ src/sspm.py
@@ -1,5 +1,20 @@
+# -*- coding: utf-8 -*-
+import os
 import ystockquote
 
 stock = ystockquote.get_price('BAC');
 
-print(stock);
+portfolioPath = os.getenv('HOME')+'/.sspm';
+try:
+	file = open(portfolioPath,'r');
+except IOError as e:
+	print('Error reading '+portfolioPath+' file.');
+	print('Create '+portfolioPath+' and add your stock information.');
+	print('In format: SYMBOL SHARES PURCHASE_PRICE');
+	print('ie. BAC 250 5.67');
+	exit();
+
+print('┌────────┬──────────┬────────────────┬───────────────┬──────────┬──────────────┐');
+print('│ SYMBOL'.ljust(11) + '│' + '  SHARES'.ljust(10) + '│' + ' PURCHASE PRICE'.ljust(16) + '│' +
+	' CURRENT PRICE'.ljust(15) + '│' + '  CHANGE'.ljust(10) + '│' +  ' TOTAL VALUE'.ljust(14) + '│');
+print('└────────┴──────────┴────────────────┴───────────────┴──────────┴──────────────┘');
